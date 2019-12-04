75bc7a6f210bf77349ad66ab60cfb60c0e253241
ngDesk <31314143+spencerfontein@users.noreply.github.com>
2019-12-04 17:34:33 +0530

    Updated file

--- appindex.php
+++ appindex.php
@@ -119,7 +119,7 @@ if($metafieldCount['count'] > 0) {
 					<button  type="submit" style="background: #03A84E; color: #fff; height:40px; width:160px;">Save widget</button>
 				</div>
 				<div class="form-group" style="margin-top: 40px">
-					Having trouble and need some help? Check out our <a href="https://support.ngdesk.com/guide">Knowledge Base</a>.	
+					Having trouble and need some help? Check out our <a href="https://support.ngdesk.com/guide" target="new">Knowledge Base</a>.	
 				</div>
 			</form>
 			
