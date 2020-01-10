632032d148d7963926f91477c2ebd8a077d47e3f
ngDesk <31314143+spencerfontein@users.noreply.github.com>
2020-01-10 10:14:47 +0530

    Updated file

--- index.php
+++ index.php
@@ -3,22 +3,24 @@
 require_once("inc/functions.php");
 
 $shop_param = $_GET['shop']; // Retrieve SHOP request parameter
-$token_param = $_GET['token']; // Retrieve SHOP request parameter
+$is_token_set = $_GET['token']; // Retrieve SHOP request parameter
 $base_url = "https://".$_SERVER['HTTP_HOST']."/shopifyapps/ngdesk/";
 $shop = explode(".myshopify.com",$shop_param)[0];
+$file = 'token.txt';
+$access_token =  file_get_contents($file);
 ?>
 
 
 <script type="text/javascript">
-var token_param = "<?php echo $token_param; ?>";
-if(token_param !== "") {
-	sessionStorage.setItem("token", "<?php echo $token_param; ?>");
-	window.location.href = '<?php echo $base_url; ?>appindex.php?shop=<?php echo $shop; ?>.myshopify.com&token=' + token_param;
+var is_token_set = "<?php echo $is_token_set; ?>";
+if(is_token_set !== "") {
+	sessionStorage.setItem("token", "<?php echo $access_token; ?>");
+	window.location.href = '<?php echo $base_url; ?>appindex.php?shop=<?php echo $shop; ?>.myshopify.com';
 } else {
 	var token = sessionStorage.getItem("token");
 	if(token !== null)
 	{
-		window.location.href = '<?php echo $base_url; ?>appindex.php?shop=<?php echo $shop; ?>.myshopify.com&token=' + token;
+		window.location.href = '<?php echo $base_url; ?>appindex.php?shop=<?php echo $shop; ?>.myshopify.com';
 	} else {
 		window.location.href = '<?php echo $base_url; ?>install.php?shop=<?php echo $shop; ?>';
 	}	
